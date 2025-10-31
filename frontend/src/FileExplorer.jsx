ceb892a31fa876b009fe52c99f145744a991c189
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-31 21:07:20 +0900

    Update FileExplorer.jsx

--- frontend/src/FileExplorer.jsx
+++ frontend/src/FileExplorer.jsx
@@ -1,32 +1,10 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
 export default function FileExplorer() {
-  const [files, setFiles] = useState([]);
-  const [newName, setNewName] = useState("");
-
-  const loadFiles = async () => {
-    const res = await axios.get("/api/files");
-    setFiles(res.data);
-  };
-
-  const createFile = async () => {
-    await axios.post("/api/files", { name: newName });
-    setNewName("");
-    loadFiles();
-  };
-
-  useEffect(() => {
-    loadFiles();
-  }, []);
-
   return (
-    <div>
-      <input value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="New file name" />
-      <button onClick={createFile}>Create</button>
-      <ul>
-        {files.map(f => <li key={f.id}>{f.name}</li>)}
-      </ul>
+    <div className="file-explorer">
+      <h3>仮想ファイルシステム</h3>
+      <p>ここにフォルダ・ファイルを表示できます。</p>
     </div>
   );
 }
