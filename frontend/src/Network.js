dbe35e637ee3ccc617c83a49405bd8f6810c3704
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-31 21:20:13 +0900

    Create Network.js

---
+++ frontend/src/Network.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Network = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <h2>仮想ネットワーク</h2>
+      <p>状態: 接続中</p>
+      <p>Ping: 42ms</p>
+      <p>データ送受信中...</p>
+    </div>
+  );
+};
+
+export default Network;
