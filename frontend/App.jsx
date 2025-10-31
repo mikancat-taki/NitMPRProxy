7d8ed804e09b0af91da56b559acf6269ba4fec87
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-31 20:27:47 +0900

    Create App.jsx

---
+++ frontend/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-3xl mb-4">Welcome to WebOS</h1>
+      <p>ログインして仮想OSを使い始めよう！</p>
+    </div>
+  );
+}
