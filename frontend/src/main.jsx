0d6d9af3a8a6fe7cede2ca2d9cf17b2a165f15fa
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-31 21:21:38 +0900

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import Desktop from "./Desktop";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Desktop />);
